4b69782097a22d840ceac2cd441ea349f32b42a5
James-Welsh <j.welsh97@protonmail.com>
2021-03-18 09:22:43 +0000

    Added constraints to input

--- app/src/main/java/com/example/notscattergories/SettingsActivity.java
+++ app/src/main/java/com/example/notscattergories/SettingsActivity.java
@@ -41,15 +41,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.confirmSettingsBtn:
-                noOfSeconds = Integer.parseInt(noOfSecondsInput.getText().toString());
-                noOfCategories = Integer.parseInt(noOfCategoriesInput.getText().toString());
-
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putInt("time", (noOfSeconds * 1000));
-                editor.putInt("categories", (noOfCategories));
-                editor.commit();
-                Toast.makeText(getApplicationContext(), "DONE", Toast.LENGTH_SHORT).show();
+                makeNewChanges();
                 break;
             case R.id.rules:
                 showRules();
@@ -57,9 +49,53 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 break;
         }
     }
+
     private void showRules() {
         DialogFragment newFragment = new RulesDialog();
         newFragment.show(getSupportFragmentManager(), "rules");
+    }
+
+    private boolean isEmpty(EditText setNumber) {
+        return !(setNumber.getText().toString().trim().length() > 0);
+    }
+
+    private void setNumberOfSeconds(SharedPreferences.Editor editor) {
+        if (!isEmpty(noOfSecondsInput)) {
+            noOfSeconds = Integer.parseInt(noOfSecondsInput.getText().toString());
+
+            if (noOfSeconds < 0) {
+                Toast.makeText(getApplicationContext(), "Time cannot be negative.", Toast.LENGTH_SHORT).show();
+            } else if (noOfSeconds > 180) {
+                noOfSeconds = 180;
+                Toast.makeText(getApplicationContext(), "Number of seconds set to 180 seconds.", Toast.LENGTH_SHORT).show();
+            }
+
+            editor.putInt("time", (noOfSeconds * 1000));
+        }
+    }
+
+    private void setNumberOfCategories(SharedPreferences.Editor editor) {
+        if (!isEmpty(noOfCategoriesInput)) {
+            noOfCategories = Integer.parseInt(noOfCategoriesInput.getText().toString());
+
+            if (noOfCategories < 0) {
+                Toast.makeText(getApplicationContext(), "Number of categories cannot be negative.", Toast.LENGTH_SHORT).show();
+            } else if (noOfCategories > 12) {
+                noOfCategories = 12;
+                Toast.makeText(getApplicationContext(), "Number of categories set to 12.", Toast.LENGTH_SHORT).show();
+            }
+
+            editor.putInt("categories", (noOfCategories));
+        }
+    }
+
+    private void makeNewChanges() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        setNumberOfSeconds(editor);
+        setNumberOfCategories(editor);
+        editor.commit();
+        Toast.makeText(getApplicationContext(), "DONE", Toast.LENGTH_SHORT).show();
     }
 
 }
